c959eb47471dd3e74c5a0015f5ae33e50bc27747
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-02-28 02:20:14 +0800

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+BOT_TOKEN = "5165466933:AAHGkpiVy6DiyWuxIDq4vEkMxNRO90Zef6s"
+API_ID = "8529843"
+SESSION_STRING = "BQATIVtEGD8nOcvHZI663BpIJPkDdQQr3738S55xZL2eOSfe3HjyCrQoQ2e3xsvLPxQ3q0nruQMnMM0T8CvSyjF-SxTRn_NnpYb_jNslEqeX1yBoM6JC2u38MOaK8Panfq4x77aPgwh9z09g9gJlUJCtFQd3sLA7Tpur9rSzZHxrnh4JlImDeBueCJ9GL0LhpgvcmZx3FInK-WwCVMjrx9xyqquai8N2-HwnsF4xCYWOC8cMwt1vm3QoEEFttLRyiti77eK_H_n6OYBMqYbmg893Pc_yHGwJsI9fH7VrgsE0ELHfHp3QlA8zsQyHIPg7j2v_KDimuvmWyPe3kX67C7SKfymgRgA"  # Check Readme to generate sessions; need for userbot
+API_HASH = "6e06fb8f7b42fb33821f272597321bc1"
+USERBOT_PREFIX = "/"
+PHONE_NUMBER = "+6285338324402"  # Need for Userbot
+SUDO_USERS_ID = "5173997839"  # Sudo users have full access to everything, don't trust anyone
+LOG_GROUP_ID = "-1001632507151"
+GBAN_LOG_GROUP_ID = "-1001632507151"
+MESSAGE_DUMP_CHAT = "-1001508910413"
+WELCOME_DELAY_KICK_SEC = "300"
+MONGO_URL = "mongodb+srv://123:123@manager.t6nkar1.mongodb.net/?retryWrites=true&w=majority"
+ARQ_API_KEY = "CISUWT-EWPYHE-YOITGT-EEKWOX-ARQ"
+ARQ_API_URL = "https://arq.hamker.in"
+LOG_MENTIONS = True
+RSS_DELAY = 300  # In seconds
+PM_PERMIT = True
